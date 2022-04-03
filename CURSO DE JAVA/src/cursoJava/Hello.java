b31659c61275467134a0cbca9bf819f89261f737
angelicapessoa <102373004+angelicapessoa@users.noreply.github.com>
2022-04-03 18:21:29 -0300

    Adicionei arquivos via upload

---
+++ CURSO DE JAVA/src/cursoJava/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package cursoJava;
+
+/**
+ * @author ap
+ * Hello Word
+ */
+public class Hello {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, word!");
+
+	}
+
+}
